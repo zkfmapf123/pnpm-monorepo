f1ac3d2df9f00bfd131186ddcf1125cc420ad248
zkfmapf123 <47292546+zkfmapf123@users.noreply.github.com>
2023-12-17 01:30:38 +0900

    add pnpm project

---
+++ apps/logins/index.js
@@ -0,0 +1,7 @@
+import { add, div, min, mul } from 'fp'
+;(() => {
+  console.log(add(10, 20))
+  console.log(min(10, 20))
+  console.log(mul(10, 20))
+  console.log(div(10, 20))
+})()
---
+++ packages/fp/index.js
@@ -0,0 +1,4 @@
+export const add = (a, b) => a + b
+export const mul = (a, b) => a * b
+export const div = (a, b) => a / b
+export const min = (a, b) => a - b
