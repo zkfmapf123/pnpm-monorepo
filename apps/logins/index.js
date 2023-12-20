bc819165672ec4dfdaed041978b65013edbac4d3
zkfmapf123 <47292546+zkfmapf123@users.noreply.github.com>
2023-12-20 18:34:47 +0900

    first commit

--- apps/logins/index.js
+++
@@ -1,7 +0,0 @@
-import { add, div, min, mul } from 'fp'
-;(() => {
-  console.log(add(10, 20))
-  console.log(min(10, 20))
-  console.log(mul(10, 20))
-  console.log(div(10, 20))
-})()
--- packages/fp/index.js
+++
@@ -1,4 +0,0 @@
-export const add = (a, b) => a + b
-export const mul = (a, b) => a * b
-export const div = (a, b) => a / b
-export const min = (a, b) => a - b
